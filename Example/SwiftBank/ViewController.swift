97e67beded769d2c50e3a21b6d2a757adbde42f5
Miguel Angel Tomairo <migueltomairo@bcp.com.pe>
2021-05-07 12:11:32 -0500

    Initial commit

---
+++ Example/SwiftBank/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  SwiftBank
+//
+//  Created by matomairo@gmail.com on 05/07/2021.
+//  Copyright (c) 2021 matomairo@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,20 @@
+# SwiftBank
+
+
+## Example
+
+Este módulo incluye un proyecto de ejemplo, luego de clonar el proyecto y ejecutar `pod install` el Workspace de ejemplo esta disponible en la carpeta "Example" 
+
+## Requirements
+
+## Installation
+
+SwiftBank esta disponible en el repositorio Git de BCP.
+
+## Author
+
+matomairo@gmail.com, migueltomairo@bcp.com.pe
+
+## License
+
+SwiftBank
